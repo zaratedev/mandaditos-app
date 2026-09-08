53467fdd16ade5ff6ccbe923a7bff08288c2446c
Jonathan Zarate <zaratedev@gmail.com>
2026-09-08 11:07:53 -0600

    Generate favicons from the courier icon
    Render the motorcycle logo to PNG favicons (16/32) and the
    apple-touch-icon (180) from favicon.svg, point the document head at the
    SVG + PNG icons, and drop the leftover Laravel favicon.ico so the
    browser tab shows the Mandaditos icon everywhere.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,8 +30,9 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
